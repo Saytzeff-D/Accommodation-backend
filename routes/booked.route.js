1844a1b0b7c933541990e6945b6e892c2b68be35
David Atanda <ololadedavid15@gmail.com>
2023-06-30 15:32:52 +0100

    authentication middleware added

--- routes/booked.route.js
+++ routes/booked.route.js
@@ -1,10 +1,12 @@
 const express = require('express')
-const { payNow, myVisits, transHistory, allBookings } = require('../controllers/booked.controller')
+const { payNow, myVisits, transHistory, allBookings, verifyPay } = require('../controllers/booked.controller')
+const { authJWT } = require('../middlewares/jwt.middleware')
 const BookedRouter = express.Router()
 
 BookedRouter.post('/pay', payNow)
-BookedRouter.post('/myVisits', myVisits)
-BookedRouter.post('/transHistory', transHistory)
+BookedRouter.get('/myVisits', authJWT, myVisits)
+BookedRouter.get('/transHistory', authJWT, transHistory)
 BookedRouter.get('/allBookings', allBookings)
+BookedRouter.post('/verifyPay', verifyPay)
 
 module.exports = BookedRouter
