6ae48ce320a7939c2ad61a92e7d00f8dddacd873
David Atanda <ololadedavid15@gmail.com>
2023-06-30 15:32:08 +0100

    verify payment

--- controllers/booked.controller.js
+++ controllers/booked.controller.js
@@ -3,7 +3,7 @@ const UserModel = require("../models/user.model");
 
 const payNow = (req, res)=>{
     let data = req.body
-    data.created_at = new Date().toLocaleTimeString()
+    data.created_at = new Date()
     let form = new BookedModel(data)
     form.save().then(()=>{
         res.status(200).json('Inserted')
@@ -38,5 +38,15 @@ const allBookings = (req, res)=>{
         res.status(300).json('Server Error')
     })
 }
+const verifyPay = (req, res)=>{
+    BookedModel.findOne({payment_ref: req.body.paymentRef}).then((booked)=>{
+        UserModel.findById(booked.user_id).then((user)=>{
+            console.log([booked, user])
+            res.json([booked, user])
+        })
+    }).catch(()=>{
+        res.status(300).json('Internal Server Error')
+    })
+}
 
-module.exports = { payNow, myVisits, transHistory, allBookings }
+module.exports = { payNow, myVisits, transHistory, allBookings, verifyPay }
